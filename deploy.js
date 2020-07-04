7cafbb6c0136ced20484a458730f20a53f170d1f
Pierre Romera <hello@pirhoo.com>
2020-07-04 22:46:29 +0200

    Added deploy script

---
+++ deploy.js
@@ -0,0 +1,15 @@
+const gh = require('gh-pages');
+const path = require('path');
+const ora = require('ora')
+const chalk = require('chalk')
+
+process.env.NODE_ENV = 'production'
+
+const spinner = ora('push dist folder to gh-pages branch...')
+const message = 'Updates [ci skip]'
+spinner.start()
+
+gh.publish(path.join(process.cwd(), 'dist'), { message }, () => {
+  spinner.stop()
+  console.log(chalk.cyan('  Push complete.\n'))
+});
