c0d897b8ec0bdaccf2f00738b3d1a7c459530107
Pierre Romera <hello@pirhoo.com>
2020-07-07 10:56:23 +0200

    Added logo in hd and favicons

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-header">
     <div class="main-header__brand">
-      <img src="../assets/logo.png" />
+      <img src="../assets/logo.png" srcset="../assets/logo.png 1x, ../assets/logo@2x.png 2x" alt="Panafrican Stories" />
     </div>
   </div>
 </template>
@@ -30,6 +30,7 @@
     &__brand img {
       height: auto;
       width: auto;
+      height: 93px;
 
       @include media-breakpoint-down(sm) {
         height: 53px;
